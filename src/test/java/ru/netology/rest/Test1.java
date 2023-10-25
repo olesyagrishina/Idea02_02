ffa4c4adb1d642d216f3427c3a824cfc6796218d
Olesya Grishina <olesyagrishina@mail.ru>
2023-10-25 20:36:08 +0300

    Gitignore added

---
+++ src/test/java/ru/netology/rest/Test1.java
@@ -0,0 +1,19 @@
+package ru.netology.rest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Test1 {
+    @Test
+    void Test01() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("winter")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("summer"));
+    }
+}
